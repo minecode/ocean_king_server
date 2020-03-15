e281b5180d5eb5f7c9755e008c6e297d1e046997
Fábio Henriques <fabiomchenriques@ua.pt>
2020-03-15 18:23:25 +0000

    create game

--- app.js
+++ app.js
@@ -80,39 +80,11 @@ app.post('/api/v1/player', function(req, res, next) {
 	);
 });
 
-app.get('/api/v1/game', function(req, res, next) {
-	res.locals.connection.query('SELECT * FROM public."Game"', function(
-		error,
-		results,
-		fields
-	) {
-		if (error) {
-			res.send(
-				JSON.stringify({
-					status: 500,
-					error: error,
-					response: null
-				})
-			);
-			//If there is error, we send the error in the error section with 500 status
-		} else {
-			res.send(
-				JSON.stringify({
-					status: 200,
-					error: null,
-					response: results
-				})
-			);
-			//If there is no error, all is good and response is 200OK.
-		}
-	});
-});
-
 app.post('/api/v1/game/:player', function(req, res, next) {
 	res.locals.connection.query(
-		'INSERT INTO public."Game" (status, created_by) VALUES ("starting", ' +
+		'INSERT INTO public."Game" (status, created_by) VALUES ({"starting"}, {' +
 			req.params.player +
-			')',
+			'})',
 		function(error, results, fields) {
 			if (error) {
 				res.send(
