7610d2fe24d7fed6960381098eb23b9cbfebf5e3
Cláudio Henriques <c.henriques@ua.pt>
2020-12-17 09:06:14 +0000

    Update index.js

--- database/index.js
+++ database/index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 mongoose.connect(
-	'mongodb://fhenriques:fabiomcH97@ds155606.mlab.com:55606/heroku_44kcv6lx',
+	'mongodb://REVOKED_fhenriques:fabiomcH97@ds155606.mlab.com:55606/heroku_44kcv6lx',
 	{ useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true }
 );
 mongoose.Promise = global.Promise;
