0eea8c868b87f20eccca67278e6ac0f572cf7d3f
Cláudio Henriques <c.henriques@ua.pt>
2020-12-17 10:19:56 +0000

    Update index.js

--- database/index.js
+++ database/index.js
@@ -1,8 +1,14 @@
 const mongoose = require('mongoose');
-mongoose.connect(
-	'mongodb://REVOKED_fhenriques:fabiomcH97@ds155606.mlab.com:55606/heroku_44kcv6lx',
-	{ useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true }
-);
+//mongoose.connect(
+//	'mongodb://REVOKED_fhenriques:fabiomcH97@ds155606.mlab.com:55606/heroku_44kcv6lx',
+//	{ useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true }
+//);
+mongoose.connect('mongodb+srv://fhenriques:fabiomcH97@cluster-44kcv6lx.r0xyz.mongodb.net/heroku_44kcv6lx?retryWrites=true&w=majority', {
+	useNewUrlParser: true,
+	useUnifiedTopology: true,
+	useCreateIndex: true
+}, () => {
+	console.log('Connected to DB!')
+})
 mongoose.Promise = global.Promise;
-
 module.exports = mongoose;
