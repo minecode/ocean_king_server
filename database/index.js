a53b1f4d3d7654eabb08876e95b0b5e9098f9c58
Cláudio Henriques <c.henriques@ua.pt>
2020-12-17 10:54:45 +0000

    Update index.js

--- database/index.js
+++ database/index.js
@@ -9,6 +9,7 @@ mongoose.connect('mongodb+srv://fhenriques:fabiomcH97@cluster-44kcv6lx.r0xyz.mon
 	useCreateIndex: true
 }, () => {
 	console.log('Connected to DB!')
+        console.log(mongoose.connection.readyState);
 })
 mongoose.Promise = global.Promise;
 module.exports = mongoose;
